da0c7d9bf94d28b7078f2dffcd237b37550f112f
Loris PUECH <puech.loris@gmail.com>
2021-12-18 11:06:22 +0100

    nft-preview-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NftPreviewCardComponent } from './components/nft-preview-card/nft-preview-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NftPreviewCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/nft-preview-card/nft-preview-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-preview-card',
+  templateUrl: './nft-preview-card.component.html',
+  styleUrls: ['./nft-preview-card.component.scss']
+})
+export class NftPreviewCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
